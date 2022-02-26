f619ff7e981aa1ee3296613b63a12cd5ee7e3cf8
KamilGrabowski <grabowskikamil@vp.pl>
2022-02-26 19:58:04 +0100

    added models

--- src/models/Invoice.js
+++ src/models/Invoice.js
@@ -3,6 +3,25 @@ const Schema = mongoose.Schema;
 
 const INVOICE_TYPES = ["paid", "pending", "draft"];
 
+const itemSchema = new Schema({
+    name:{
+        type: String,
+        defualt: null,
+    },
+    quantity:{
+        type: String,
+        defualt: null,
+    },
+    price:{
+        type: String,
+        defualt: null,
+    },
+    total:{
+        type: String,
+        defualt: null,
+    }
+});
+
 const NoteSchema = new Schema({
   type: {
     type: String,
@@ -12,13 +31,56 @@ const NoteSchema = new Schema({
   from: [
       street:{
         type: String,
-        required: true,
+        defualt: null,
       },
       city:{
         type: String,
-        required: true,
-      }
+        defualt: null,
+      },
+      post_code:{
+        type: String,
+        defualt: null,
+      },
+      country:{
+        type: String,
+        defualt: null,
+      },
   ],
+  to: [
+    name:{
+      type: String,
+      defualt: null,
+    },
+    email:{
+      type: String,
+      defualt: null,
+    },
+    city:{
+        type: String,
+        defualt: null,
+      },
+    post_code:{
+      type: String,
+      defualt: null,
+    },
+    country:{
+      type: String,
+      defualt: null,
+    },
+  ],
+  invoice_date:{
+    type: String,
+    defualt: null,
+  },
+  payment_term:{
+    type: String,
+    defualt: null,
+  },
+  description:{
+    type: String,
+    defualt: null,
+  },
+  items_list:[itemSchema],
   userID: {
     type: String,
     required: true,
